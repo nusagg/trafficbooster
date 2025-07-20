bc2823597f302ce1f981b8e0eba838735f9f6402
nusagg <seogokil190402@gmail.com>
2025-07-20 00:04:18 -0700

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -5,29 +5,31 @@ import threading
 
 config = configparser.ConfigParser()
 config.read('config.ini')
-TOKEN = config['telegram']['7511600391:AAF1xff06DqkYuVA5B2FiEi96rlmYzimslc']
-7958271584 = int(config['telegram']['7958271584'])
+
+# Ambil token dan admin ID dari config
+TOKEN = config['telegram']['bot_token']
+ADMIN_ID = int(config['telegram']['admin_id'])
 
 bot = telebot.TeleBot(TOKEN)
 process = None
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    if message.from_user.id != 7958271584:
+    if message.from_user.id != ADMIN_ID:
         return
     bot.reply_to(message, "/run - Jalankan booster\n/status - Cek status\n/stop - Hentikan booster")
 
 @bot.message_handler(commands=['run'])
 def handle_run(message):
     global process
-    if message.from_user.id != 7958271584:
+    if message.from_user.id != ADMIN_ID:
         return
     msg = bot.reply_to(message, "Masukkan target|threads|delay (contoh: https://site.com|10|2):")
     bot.register_next_step_handler(msg, run_booster)
 
 def run_booster(message):
     global process
-    if message.from_user.id != 7958271584:
+    if message.from_user.id != ADMIN_ID:
         return
     try:
         target, threads, delay = message.text.split('|')
@@ -44,7 +46,7 @@ def run_booster(message):
 @bot.message_handler(commands=['status'])
 def handle_status(message):
     global process
-    if message.from_user.id != 7958271584:
+    if message.from_user.id != ADMIN_ID:
         return
     status = "Aktif" if process and process.poll() is None else "Tidak aktif"
     bot.send_message(message.chat.id, f"Status: {status}")
@@ -52,7 +54,7 @@ def handle_status(message):
 @bot.message_handler(commands=['stop'])
 def handle_stop(message):
     global process
-    if message.from_user.id != 7958271584:
+    if message.from_user.id != ADMIN_ID:
         return
     if process:
         process.terminate()
